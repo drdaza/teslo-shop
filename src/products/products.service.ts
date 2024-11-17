fa5cc2d7226d7ad4c239dcda38525a0ad2262c78
David Daza <drdaza18@gmail.com>
2024-11-17 16:52:26 +0100

    added basic endpoints and actions for products entity

---
+++ src/products/products.service.ts
@@ -0,0 +1,86 @@
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+
+  private readonly logger = new Logger(ProductsService.name);
+  
+  constructor(
+
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+
+  ) {}
+  
+  async create(createProductDto: CreateProductDto) {
+
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+
+      return product;
+    }
+    catch (error) {
+      this.handleDbExpections(error);
+    }
+
+  }
+
+  async findAll() {
+    try {
+      const products = await this.productRepository.find({});
+
+      return products;
+    } catch (error) {
+      this.handleDbExpections(error);
+    }
+  }
+
+  async findOne(id: string) {
+      const product = await this.productRepository.findOneBy({ id });      
+      if (!product) {        
+        throw new NotFoundException('Product not found');
+      }
+      
+      return product;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    try {
+      await this.productRepository.remove(product);
+
+    } catch (error) {
+      this.handleDbExpections(error);
+    }
+
+    return `the product with id: ${id} has been eliminated `;
+  }
+
+  private getTermType(term: string) {
+    if (!isNaN(+term)) {
+      return 'number';
+    } else {
+      return 'string';
+    }
+  }
+
+  private handleDbExpections (error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    } 
+    
+    this.logger.error(error);
+        
+    throw new InternalServerErrorException('unexpected error, check logs');
+  }
+}
